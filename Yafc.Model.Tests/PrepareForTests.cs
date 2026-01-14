64bb72d94584786f23bd676ed9f8d1b10ecf9ed3
Dorus <dpeelen@gmail.com>
2026-01-14 16:14:11 +0100

    chore: formatting

--- Yafc.Model.Tests/PrepareForTests.cs
+++ Yafc.Model.Tests/PrepareForTests.cs
@@ -8,6 +8,7 @@ using Yafc.Model.Tests;
 [assembly: TestFramework("Yafc.Model.Tests." + nameof(PrepareForTests), "Yafc.Model.Tests")]
 
 namespace Yafc.Model.Tests;
+
 public class PrepareForTests : XunitTestFramework {
     public PrepareForTests(IMessageSink messageSink)
         : base(messageSink) => Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
